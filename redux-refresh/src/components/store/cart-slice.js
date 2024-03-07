edd6f5279a557c9b114a91a3bcda0d2e7b9ef6bb
Zach Wilkins <zach.d.wilkins@gmail.com>
2024-03-06 19:24:54 -0500

    end of section 20

--- redux-refresh/src/components/store/cart-slice.js
+++ redux-refresh/src/components/store/cart-slice.js
@@ -1,15 +1,22 @@
 import { createSlice } from "@reduxjs/toolkit";
+
 export const cartSlice = createSlice({
   name: "cart",
   initialState: {
     items: [],
     totalQuantity: 0,
+    changed: false,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
+      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -27,6 +34,7 @@ export const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
+      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
@@ -36,5 +44,7 @@ export const cartSlice = createSlice({
     },
   },
 });
+
+
 
 export const cartActions = cartSlice.actions;
