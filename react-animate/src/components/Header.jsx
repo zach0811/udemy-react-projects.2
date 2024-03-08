fbfbf7d405b7432f6a8c4ce5e40e2e75281916ce
Zach Wilkins <zach.d.wilkins@gmail.com>
2024-03-08 07:55:33 -0500

    section 26-animating elements

--- react-animate/src/components/Header.jsx
+++ react-animate/src/components/Header.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
+import { AnimatePresence, motion } from "framer-motion";
 
-import NewChallenge from './NewChallenge.jsx';
+import NewChallenge from "./NewChallenge.jsx";
 
 export default function Header() {
   const [isCreatingNewChallenge, setIsCreatingNewChallenge] = useState();
@@ -15,13 +16,20 @@ export default function Header() {
 
   return (
     <>
-      {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      <AnimatePresence>
+        {isCreatingNewChallenge && <NewChallenge onDone={handleDone} />}
+      </AnimatePresence>
 
       <header id="main-header">
         <h1>Your Challenges</h1>
-        <button onClick={handleStartAddNewChallenge} className="button">
+        <motion.button
+          onClick={handleStartAddNewChallenge}
+          transition={{ type: "spring", stiffness: 100 }}
+          className="button"
+          whileHover={{ scale: 1.1 }}
+        >
           Add Challenge
-        </button>
+        </motion.button>
       </header>
     </>
   );
