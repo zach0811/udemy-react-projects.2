f527a6540e1cb868d9f4fa2365bb48d643f03ba5
Zach Wilkins <zach.d.wilkins@gmail.com>
2024-03-12 07:05:06 -0400

    section 28

---
+++ bonus-hooks-section/src/containers/Products.js
@@ -0,0 +1,25 @@
+import { useStore } from "../hooks-store/store";
+
+import ProductItem from "../components/Products/ProductItem";
+import "./Products.css";
+
+const Products = (props) => {
+  // const productList = useSelector(state => state.shop.products);
+  // const productList = useContext(ProductsContext).products;
+  const state = useStore()[0];
+  return (
+    <ul className="products-list">
+      {state.products.map((prod) => (
+        <ProductItem
+          key={prod.id}
+          id={prod.id}
+          title={prod.title}
+          description={prod.description}
+          isFav={prod.isFavorite}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default Products;
